da0e35f3c87df25319e68e68702065b7bede8c9f
dkfqkahs35-spec <dkfqkahs35@gmail.com>
2025-08-13 09:42:35 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>마음앵커 ⚓</h1>
+      <p>정신건강 회복을 위한 PWA 앱입니다.</p>
+    </div>
+  );
+}
+
+export default App;
