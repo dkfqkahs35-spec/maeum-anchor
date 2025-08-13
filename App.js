d85120c092ae252e794be25b3ff3f7753cace766
dkfqkahs35-spec <dkfqkahs35@gmail.com>
2025-08-13 09:48:55 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+    <div style={{ fontFamily: 'sans-serif', padding: 20 }}>
       <h1>마음앵커 ⚓</h1>
-      <p>정신건강 회복을 위한 PWA 앱입니다.</p>
+      <p>정신건강 회복을 위한 개인 맞춤 PWA입니다.</p>
+      <p>오늘 기분을 기록하고, 나만의 치유 루틴을 만들어 보세요.</p>
     </div>
   );
 }
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// PWA 서비스 워커 등록
+serviceWorkerRegistration.register();
